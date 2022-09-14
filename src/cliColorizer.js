b3886c09a6b92cc24f360954c49cef2321ef3dc2
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-14 10:05:39 -0400

    Add Function that Colors the Supplied Text

---
+++ src/cliColorizer.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk')
+
+module.exports.colorize = ((type, text) => {
+    let coloredText = "";
+    switch(type){
+        case 'h':
+            coloredText = chalk.inverse.bold(text)
+            break
+        case 'cmd': 
+            coloredText = chalk.greenBright(text)
+            break
+        case 'flg':
+            coloredText = chalk.blueBright(text)
+            break
+        case 'src':
+            coloredText = chalk.bold.yellowBright(text)
+            break
+        case 'err':
+            coloredText = chalk.redBright.bold.inverse(text)
+            break
+        case 'success':
+            coloredText = chalk.green(text)
+            break
+    }
+
+    return coloredText;
+})
