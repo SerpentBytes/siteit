270e891e627163f7960fa9ab601f07cb24725204
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-14 10:04:40 -0400

    Add Main Function Which Always Executed First

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-const { dispatch } = require('./utlility')
+const { dispatch, handleImproperUsage } = require('./utlility')
 
 module.exports.main = (args) => {
-    if(args.length < 3){
-        console.log("insufficient or no arguments supplied")
-    } else if(args.length === 3 && (args[2] === "-v" || args[2] === "--version")){
+   if(args.length === 3 && (args[2] === "-v" || args[2] === "--version")){
         dispatch("-v")
     } else if(args.length === 3 && (args[2] === "-h" || args[2] === "--help")){
         dispatch("-h")
+    } else if(args[2] === "-i" || args[2] === "--input"){
+        dispatch('-i', args)
+    } else {
+        handleImproperUsage()
     }
 }
