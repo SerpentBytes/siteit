3e74c16328875312b170ea9544304b71473b5329
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-19 13:23:11 -0400

    Main formatted using pretty

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-const { dispatch, handleImproperUsage } = require('./utility')
+const { dispatch, handleImproperUsage } = require("./utility");
 
 module.exports.main = (args) => {
-   if(args.length === 3 && (args[2] === "-v" || args[2] === "--version")){
-        dispatch("-v")
-    } else if(args.length === 3 && (args[2] === "-h" || args[2] === "--help")){
-        dispatch("-h")
-    } else if(args[2] === "-i" || args[2] === "--input"){
-        dispatch('-i', args)
-    } else {
-        handleImproperUsage()
-    }
-}
+  if (args.length === 3 && (args[2] === "-v" || args[2] === "--version")) {
+    dispatch("-v");
+  } else if (args.length === 3 && (args[2] === "-h" || args[2] === "--help")) {
+    dispatch("-h");
+  } else if (args[2] === "-i" || args[2] === "--input") {
+    dispatch("-i", args);
+  } else {
+    handleImproperUsage();
+  }
+};
