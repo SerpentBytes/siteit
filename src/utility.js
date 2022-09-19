40316bfac6d04e762a1616b8a1a019bc1fa058bb
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-19 19:11:14 -0400

    Update processInput and add comments

--- src/utility.js
+++ src/utility.js
@@ -12,6 +12,8 @@ const {
 } = require("./cliDisplay");
 const { generateHTML, generateIndexFile } = require("./htmlGenerator");
 
+/* This function is executed when user incorrectly makes use of the
+available options */
 const handleImproperUsage = (option) => {
   switch (option) {
     case "-i":
@@ -20,13 +22,18 @@ const handleImproperUsage = (option) => {
       );
       break;
     default:
-      console.log(`Usage: ${cmd()} ${flag('[flag]')} ${src('[source]')}`);
+      console.log(`Usage: ${cmd()} ${flag("[flag]")} ${src("[source]")}`);
   }
 };
+/* This function is ultimately executed to display tool name and version when the user
+ runs the tool with -v or --version option */
+
 const displayNameVersion = () => {
   console.log(`Name: ${packageJson.name}\nVersion: ${packageJson.version}`);
 };
 
+/* This function is ultimately executed to display the usage guide for when -h or --help option is
+used */
 const displayManual = () => {
   console.log(`   
 ${hdg("SiteIt - USAGE MANUAL")}
@@ -58,21 +65,27 @@ Common SiteIt Flag Options
                         *** END OF MANUAL ***                   
     `);
 };
+/* distManager is responsible for ensuring no existing outdirectory exists.
+If an output directory is found, it is recursively deleted, and a new one is created
+in its place.*/
 
 const distManager = () => {
   try {
     console.log(secondary("-- Attempting to create output directory --"));
     fs.mkdirSync(path.join(__dirname, "../dist"));
   } catch (e) {
+    // checking if directory already exists
     if (e.code === "EEXIST") {
       console.log(
         secondary("-- Output directory exists. Attempting to delete it --")
       );
+      // delete the existing directory
       fs.rmSync(path.join(__dirname, "../dist"), {
         recursive: true,
         force: true,
       });
       console.log(success(`-- Pre-existing output directory deleted --`));
+      // create a new dist directory
       fs.mkdirSync(path.join(__dirname, "../dist"));
     }
   }
@@ -80,15 +93,18 @@ const distManager = () => {
     success(`-- DONE creating output directory: ${path.resolve("../dist")} --`)
   );
 };
+/* processFile accepts a file as a parameter. It is responsible for processing
+a single input file provided by user. It reads the content of the file, and
+and invokes a function call to generateHTML with srcPath, and file contents */
+const processFile = (srcPath) => {
+  distManager(); // check if output directory exists, if it does delete it, and create a new one
 
-const processFile = (file) => {
-  distManager();
-  let filePath = path.isAbsolute(file) ? file : path.resolve(file);
+  // if the source path is relative, convert it to absolute
+  let filePath = path.isAbsolute(srcPath) ? srcPath : path.resolve(srcPath);
   try {
     let content = fs.readFileSync(filePath, { encoding: "utf8" });
-    generateHTML(file, content);
+    generateHTML(srcPath, content); // generate HTML file based on paramaters supplied
   } catch (e) {
-    console.log(e)
     console.error(
       `${err(
         "-- ERROR: error reading file. Make sure file supplied exists --"
@@ -96,6 +112,12 @@ const processFile = (file) => {
     );
   }
 };
+
+/* processDir accepts a directory as a parameter. It is responsible for processing
+a single directory containing txt files, provided by the user. It reads the content of each the file
+within the directory. It maps through an array of files, and invokes a call
+to generateHTML, passing it the file path and its content, for every file.
+*/
 
 const processDir = (dir) => {
   let files = [];
@@ -105,47 +127,61 @@ const processDir = (dir) => {
     files = fs.readdirSync(dirPath);
 
     files.map((file) => {
-      file.replace(' ', '\ ');
       let content = fs.readFileSync(`${dirPath}/${file}`, { encoding: "utf8" });
       generateHTML(file, content);
-    })
-
+    });
   } catch (e) {
-    console.log(err(` ${e} -- ERROR -- Invalid or unsupported files supplied --`))
+    console.log(
+      err(` ${e} -- ERROR -- Invalid or unsupported files supplied --`)
+    );
   }
+  // generate an Index.html file using the files supplied
   generateIndexFile(files);
 };
+
+/* processInput accepts arguments passed by the dispatch function. It is 
+responsible to evaluate whether the source supplied is a source or a 
+directory. Upon checking it invokes the approrpiate function responsible for the handling
+input type, or prints an error message, if user supplied invalid parameter.
+*/
+
 const processInput = (args) => {
-  let src = args[1].splice(3).toString().replace(",", " ");
+  // Remove "-i" option from the parameter and take care of spaces in source 
+  let src = args[1].splice(1).toString().replace(",", " "); 
+
+  // if user does not supply a file, print an error message 
   if (src.length === 0) {
     console.error(err("-- ERROR: No source supplied --"));
   } else {
     try {
+      // check if the source is a file
       if (fs.statSync(src).isFile()) {
-        processFile(src);
+        processFile(src); // process the file
+      // check if source is a directory
       } else if (fs.statSync(src).isDirectory()) {
-        processDir(src);
+        processDir(src); // process the directory
       }
     } catch (e) {
-      console.error(
-        `${err("-- Invalid or multiple source supplied --")}`
-      );
-      console.log(e)
-      handleImproperUsage();
+      console.error(`${err("-- Invalid or multiple source supplied --")}`);
+      handleImproperUsage(); // print proper usage message 
     }
   }
 };
 
+/* dispatch function name is inspired by Redux dispatch function. 
+Here, dispatch is responsible for dispatching the job to the right
+function based on option supplied by user
+*/
 const dispatch = (...args) => {
   switch (args[0]) {
     case "-v":
-      displayNameVersion();
+      displayNameVersion(); // display name and version
       break;
     case "-h":
-      displayManual();
+      displayManual();  // display guide
       break;
     case "-i":
-      processInput(args);
+      processInput(args); // process input
       break;
   }
 };
