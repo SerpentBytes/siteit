00d3c5e9d667cf67708ec17c6c00afeab0ae8a04
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-19 19:23:10 -0400

    Add comments

--- src/cliColorizer.js
+++ src/cliColorizer.js
@@ -1,27 +1,28 @@
-const chalk = require("chalk");
+const chalk = require("chalk"); // module to change colour and formatting
+// of terminal output
 
 module.exports.colorize = (type, text) => {
   let coloredText = "";
   switch (type) {
-    case "h":
+    case "h": // heading
       coloredText = chalk.inverse.bold(text);
       break;
-    case "cmd":
+    case "cmd": // command
       coloredText = chalk.greenBright(text);
       break;
-    case "flg":
+    case "flg": // flag / option
       coloredText = chalk.blueBright(text);
       break;
-    case "src":
+    case "src": // source
       coloredText = chalk.bold.yellowBright(text);
       break;
-    case "err":
+    case "err": // error
       coloredText = chalk.redBright.bold.inverse(text);
       break;
-    case "success":
+    case "success": // success messages
       coloredText = chalk.green(text);
       break;
-    case "secondary":
+    case "secondary": // message worth noting but not important
       coloredText = chalk.yellow(text);
       break;
   }
