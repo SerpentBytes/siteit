8198cfa6f194f370af563b6380f160784ea31533
TD <50856799+SerpentBytes@users.noreply.github.com>
2022-09-19 14:16:36 -0400

    Fix code formatting

--- src/cliColorizer.js
+++ src/cliColorizer.js
@@ -1,30 +1,30 @@
-const chalk = require('chalk')
+const chalk = require("chalk");
 
-module.exports.colorize = ((type, text) => {
-    let coloredText = "";
-    switch(type){
-        case 'h':
-            coloredText = chalk.inverse.bold(text)
-            break
-        case 'cmd': 
-            coloredText = chalk.greenBright(text)
-            break
-        case 'flg':
-            coloredText = chalk.blueBright(text)
-            break
-        case 'src':
-            coloredText = chalk.bold.yellowBright(text)
-            break
-        case 'err':
-            coloredText = chalk.redBright.bold.inverse(text)
-            break
-        case 'success':
-            coloredText = chalk.green(text)
-            break
-        case 'secondary':
-            coloredText = chalk.yellow(text)
-            break
-    }
+module.exports.colorize = (type, text) => {
+  let coloredText = "";
+  switch (type) {
+    case "h":
+      coloredText = chalk.inverse.bold(text);
+      break;
+    case "cmd":
+      coloredText = chalk.greenBright(text);
+      break;
+    case "flg":
+      coloredText = chalk.blueBright(text);
+      break;
+    case "src":
+      coloredText = chalk.bold.yellowBright(text);
+      break;
+    case "err":
+      coloredText = chalk.redBright.bold.inverse(text);
+      break;
+    case "success":
+      coloredText = chalk.green(text);
+      break;
+    case "secondary":
+      coloredText = chalk.yellow(text);
+      break;
+  }
 
-    return coloredText;
-})
+  return coloredText;
+};
--- src/cliDisplay.js
+++ src/cliDisplay.js
@@ -1,18 +1,18 @@
-const packageJson = require('../package.json')
-const { colorize } = require('./cliColorizer')
+const packageJson = require("../package.json");
+const { colorize } = require("./cliColorizer");
 
-const cmd = () => colorize('cmd',Object.keys(packageJson.bin))
+const cmd = () => colorize("cmd", Object.keys(packageJson.bin));
 
-const flag = (value) => colorize('flg', value)
+const flag = (value) => colorize("flg", value);
 
-const src = (value) => colorize('src', value)
+const src = (value) => colorize("src", value);
 
-const hdg = (value) => colorize('h', value)
+const hdg = (value) => colorize("h", value);
 
-const err = (value) => colorize('err', value)
+const err = (value) => colorize("err", value);
 
-const success = (value) => colorize('success', value)
+const success = (value) => colorize("success", value);
 
-const secondary = (value) => colorize('secondary', value)
+const secondary = (value) => colorize("secondary", value);
 
-module.exports = { cmd, flag, src, hdg, err, success, secondary }
+module.exports = { cmd, flag, src, hdg, err, success, secondary };
